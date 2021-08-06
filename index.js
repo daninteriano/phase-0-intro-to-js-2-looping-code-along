46beecd52f5ff96c137e2fd72adba03afc3b2785
Daniel Interiano <dannyboyint@gmail.com>
2021-08-06 16:01:39 -0400

    commit tree 34cd9b281747faab8b6837fdd6c4f7c06c9cf177

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solutions in this file
+/*for (let age= 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "droll"];
+
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+    }
+    return gifts;
+}
+wrapGifts(gifts);*/
+
+//let students = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards (students, event) {
+    let newArray = [];
+    for (let i = 0; i < students.length; i++){
+       newArray.push(`Thank you, ${students[i]}, for the wonderful ${event} gift!`)
+    }
+       return newArray;
+};
+
+//writeCards(["Lisa", "Kaitlin", "Jan"], 'surprise');
+
+
+function countDown(num) {
+    let i = -1
+    while(i < num){
+        console.log(num--);
+    }
+    return num;
+}
+
+//countDown(10);
+//countDown(4);
